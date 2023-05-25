c45748f1fe78f69e1e713532f8130a9dc6d6bc7f
Tien-Duc Cao <tienduc.cao@dashlane.com>
2023-05-25 16:56:15 +0200

    feat: A simple and not optimal tweak to let the model output one single response instead of chat mode

--- examples/redpajama/main-redpajama-chat.cpp
+++ examples/redpajama/main-redpajama-chat.cpp
@@ -29,6 +29,7 @@ static console_state con_st;
 static gptneox_context ** g_ctx;
 
 static bool is_interacting = false;
+static bool is_processing = true;
 
 #if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__)) || defined (_WIN32)
 void sigint_handler(int signo) {
@@ -48,73 +49,73 @@ void sigint_handler(int signo) {
 int main(int argc, char ** argv) {
     gpt_params params;
     params.model = "./examples/redpajama/models/pythia/ggml-RedPajama-INCITE-Chat-3B-v1-f16.bin";
-    
+
     if (gpt_params_parse(argc, argv, params) == false) {
         return 1;
     }
-    
+
     // save choice to use color for later
     // (note for later: this is a slightly awkward choice)
     con_st.use_color = params.use_color;
-    
+
 #if defined (_WIN32)
     win32_console_init(params.use_color);
 #endif
-    
+
     if (params.perplexity) {
         printf("\n************\n");
         printf("%s: please use the 'perplexity' tool for perplexity calculations\n", __func__);
         printf("************\n\n");
-        
+
         return 0;
     }
-    
+
     if (params.embedding) {
         printf("\n************\n");
         printf("%s: please use the 'embedding' tool for embedding calculations\n", __func__);
         printf("************\n\n");
-        
+
         return 0;
     }
-    
+
     if (params.n_ctx > 2048) {
         fprintf(stderr, "%s: warning: model does not support context sizes greater than 2048 tokens (%d specified);"
                 "expect poor results\n", __func__, params.n_ctx);
     }
-    
+
     if (params.seed <= 0) {
         params.seed = time(NULL);
     }
-    
+
     fprintf(stderr, "%s: seed = %d\n", __func__, params.seed);
-    
+
     std::mt19937 rng(params.seed);
     if (params.random_prompt) {
         params.prompt = gpt_random_prompt(rng);
     }
-    
+
     gptneox_context * ctx;
     g_ctx = &ctx;
-    
+
     // load the model
     {
         auto lparams = gptneox_context_default_params();
-        
+
         lparams.n_ctx      = params.n_ctx;
         lparams.n_parts    = params.n_parts;
         lparams.seed       = params.seed;
         lparams.f16_kv     = params.memory_f16;
         lparams.use_mmap   = params.use_mmap;
         lparams.use_mlock  = params.use_mlock;
-        
+
         ctx = gptneox_init_from_file(params.model.c_str(), lparams);
-        
+
         if (ctx == NULL) {
             fprintf(stderr, "%s: error: failed to load model '%s'\n", __func__, params.model.c_str());
             return 1;
         }
     }
-    
+
     if (!params.lora_adapter.empty()) {
         int err = gptneox_apply_lora_from_file(ctx,
                                                params.lora_adapter.c_str(),
@@ -125,35 +126,35 @@ int main(int argc, char ** argv) {
             return 1;
         }
     }
-    
+
     // print system information
     {
         fprintf(stderr, "\n");
         fprintf(stderr, "system_info: n_threads = %d / %d | %s\n",
                 params.n_threads, std::thread::hardware_concurrency(), gptneox_print_system_info());
     }
-    
+
     // determine the maximum memory usage needed to do inference for the given n_batch and n_predict parameters
     if (params.mem_test) {
         {
             const std::vector<gptneox_token> tmp(params.n_batch, 0);
             gptneox_eval(ctx, tmp.data(), tmp.size(), 0, params.n_threads);
         }
-        
+
         {
             const std::vector<gptneox_token> tmp = { 0, };
             gptneox_eval(ctx, tmp.data(), tmp.size(), params.n_predict - 1, params.n_threads);
         }
-        
+
         gptneox_print_timings(ctx);
         gptneox_free(ctx);
-        
+
         return 0;
     }
 
     // Always interactive for RedPajama chat model
     params.interactive = true;
-    
+
     if (params.interactive) {
 #if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
         struct sigaction sigint_action;
@@ -169,13 +170,13 @@ int main(int argc, char ** argv) {
         params.temp, params.top_k, params.top_p, params.repeat_last_n, params.repeat_penalty);
     fprintf(stderr, "generate: n_ctx = %d, n_batch = %d, n_predict = %d, n_keep = %d\n", params.n_ctx, params.n_batch, params.n_predict, params.n_keep);
     fprintf(stderr, "\n\n");
-    
+
     // TODO: replace with ring-buffer
     std::vector<gptneox_token> last_n_tokens = std::vector<gptneox_token>();
     //std::fill(last_n_tokens.begin(), last_n_tokens.end(), 0);
-    
+
     set_console_color(con_st, CONSOLE_COLOR_PROMPT);
-    
+
     if (params.interactive) {
         printf("== Running in interactive mode. ==\n"
 #if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__)) || defined (_WIN32)
@@ -184,23 +185,23 @@ int main(int argc, char ** argv) {
                " - Press Return to return control to RedPajama.\n"
                " - If you want to submit another line, end your input in '\\'.\n\n");
     }
-    
+
     const int32_t top_k          = params.top_k;
     const float   top_p          = params.top_p;
     const float   temp           = params.temp;
     const float   repeat_penalty = params.repeat_penalty;
-    
+
     // Chat loop
-    while (true) {
+    while (is_processing) {
         is_interacting = true;
-        
+
         int n_past = 0;
-        
+
         // Get input
-        
+
         // potentially set color to indicate we are taking user input
         set_console_color(con_st, CONSOLE_COLOR_USER_INPUT);
-        
+
 #if defined (_WIN32)
         // Windows: must reactivate sigint handler after each signal
         signal(SIGINT, sigint_handler);
@@ -210,49 +211,52 @@ int main(int argc, char ** argv) {
             printf("\n<human>: ");
         }
 
-        std::string buffer;
-        if (!params.input_prefix.empty()) {
-            buffer += params.input_prefix;
-            printf("%s", buffer.c_str());
-        }
+        std::string buffer = params.prompt;
 
-        std::string line;
-        bool another_line = true;
-        do {
-#if defined(_WIN32)
-            std::wstring wline;
-            if (!std::getline(std::wcin, wline)) {
-                // input stream is bad or EOF received
-                return 0;
-            }
-            win32_utf8_encode(wline, line);
-#else
-            if (!std::getline(std::cin, line)) {
-                // input stream is bad or EOF received
-                return 0;
-            }
-#endif
-            if (line.empty() || line.back() != '\\') {
-                another_line = false;
-            } else {
-                line.pop_back(); // Remove the continue character
-            }
-            buffer += line;
-            if (another_line) {
-                buffer += '\n';
-            }
-        } while (another_line);
-        
+        is_processing = false;
+
+        //if (!params.input_prefix.empty()) {
+            //buffer += params.input_prefix;
+            //printf("%s", buffer.c_str());
+        //}
+
+        //std::string line;
+        //bool another_line = true;
+        //do {
+//#if defined(_WIN32)
+            //std::wstring wline;
+            //if (!std::getline(std::wcin, wline)) {
+                //// input stream is bad or EOF received
+                //return 0;
+            //}
+            //win32_utf8_encode(wline, line);
+//#else
+            //if (!std::getline(std::cin, line)) {
+                //// input stream is bad or EOF received
+                //return 0;
+            //}
+//#endif
+            //if (line.empty() || line.back() != '\\') {
+                //another_line = false;
+            //} else {
+                //line.pop_back(); // Remove the continue character
+            //}
+            //buffer += line;
+            //if (another_line) {
+                //buffer += '\n';
+            //}
+        //} while (another_line);
+
         is_interacting = false;
-        
+
         // done taking input, reset color
         set_console_color(con_st, CONSOLE_COLOR_DEFAULT);
-        
+
         // Check for input
         if (buffer.length() <= 0) {
             continue; // Restart loop for input
         }
-        
+
         // Tokenize prompt with RedPajama special tokens
 
         auto prompt_embd = ::gptneox_tokenize(ctx, buffer, false);
@@ -262,7 +266,7 @@ int main(int argc, char ** argv) {
         embd_inp.push_back(gptneox_str_to_token(ctx, "<"));
         embd_inp.push_back(gptneox_str_to_token(ctx, "human"));
         embd_inp.push_back(gptneox_str_to_token(ctx, ">:"));
-        
+
         embd_inp.insert(embd_inp.end(), prompt_embd.begin(), prompt_embd.end());
 
         // Redpajama: insert special tokens for OA. (postfix)
@@ -270,8 +274,8 @@ int main(int argc, char ** argv) {
         embd_inp.push_back(gptneox_str_to_token(ctx, "<"));
         embd_inp.push_back(gptneox_str_to_token(ctx, "bot"));
         embd_inp.push_back(gptneox_str_to_token(ctx, ">:"));
-       
-        
+
+
         // Verbose prompt
         if (params.verbose_prompt) {
             fprintf(stderr, "\n");
@@ -282,7 +286,7 @@ int main(int argc, char ** argv) {
             }
             fprintf(stderr, "\n");
         }
-        
+
         // How many tokens to generate - check if theres space in context for atleast one token (or batch size tokens?)
         auto inp_size = embd_inp.size();
         auto space = params.n_ctx - inp_size;
@@ -300,10 +304,10 @@ int main(int argc, char ** argv) {
             }
             n_past += n_eval;
         }
-        
+
         const int n_ctx = gptneox_n_ctx(ctx);
         const int n_vocab = gptneox_n_vocab(ctx);
-        
+
         const float   temp            = params.temp;
         const int32_t top_k           = params.top_k <= 0 ? gptneox_n_vocab(ctx) : params.top_k;
         const float   top_p           = params.top_p;
@@ -317,18 +321,18 @@ int main(int argc, char ** argv) {
         const float   mirostat_tau    = params.mirostat_tau;
         const float   mirostat_eta    = params.mirostat_eta;
         const bool    penalize_nl     = params.penalize_nl;
-        
+
         // Eval until space runs out
         auto out_count = 0;
-        
+
         printf("<bot>:");
         while (space > 0) {
             // Get token
             gptneox_token id = 0;
-            
+
             {
                 auto logits = gptneox_get_logits(ctx);
-                
+
                 // Apply params.logit_bias map
                 for (auto it = params.logit_bias.begin(); it != params.logit_bias.end(); it++) {
                     logits[it->first] += it->second;
@@ -380,7 +384,7 @@ int main(int argc, char ** argv) {
                     }
                 }
             }
-            
+
             // Inc out count and dec space
             out_count += 1;
             space -= 1;
@@ -389,9 +393,9 @@ int main(int argc, char ** argv) {
             if (last_n_tokens.size() > params.repeat_last_n) {
                 last_n_tokens.erase(last_n_tokens.begin());
             }
-            // Redpajama: check if the interactive is done. 
+            // Redpajama: check if the interactive is done.
             //std::cout<<" last_n_tokens.size: "<< last_n_tokens[0] <<" "<< last_n_tokens[1] <<" "<< last_n_tokens[2] << std::endl;
-            if (last_n_tokens.size()==3 && last_n_tokens[0]==gptneox_str_to_token(ctx, "<") 
+            if (last_n_tokens.size()==3 && last_n_tokens[0]==gptneox_str_to_token(ctx, "<")
             && last_n_tokens[1]==gptneox_str_to_token(ctx, "human") && last_n_tokens[2]==gptneox_str_to_token(ctx, ">:")){
                 space = 0;
                 continue;
@@ -408,8 +412,8 @@ int main(int argc, char ** argv) {
             }
             // Convert token to string and display
             // printf("%s(%d)", gptneox_token_to_str(ctx, id), id);
-            
-            
+
+
             if (last_n_tokens[2]==gptneox_str_to_token(ctx, "<")){
                 ;
             }
@@ -442,11 +446,11 @@ int main(int argc, char ** argv) {
             // Check for user interrupt
             if (is_interacting) { space = 0; }
         }
-        printf("\n"); 
+        printf("\n");
         //printf("\n %d", space);
         fflush(stdout);
     }
-    
+
 #if defined (_WIN32)
     signal(SIGINT, SIG_DFL);
 #endif
@@ -458,10 +462,10 @@ int main(int argc, char ** argv) {
 
     return 0;
 }
-    
-    
-    
-    
-    
+
+
+
+
+
 
 
